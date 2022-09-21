03b4fea08a93272116afee9901a5421d15e1edbd
unknown <iamwiqi@gmail.com>
2022-09-21 21:53:59 +0500

    minor fixes

--- src/components/basic/Item.js
+++ src/components/basic/Item.js
@@ -4,7 +4,7 @@ const Item = (Item) => {
 
         <div className="bg-white border border-gray-100 transition transform duration-700 hover:shadow-xl hover:scale-105 p-4 rounded-lg relative">
             <span className="bg-red-100 border border-red-500 rounded-full text-primary text-sm poppins px-4 py-1 inline-block mb-4 ">
-                s{Item.details.category}
+                {Item.details.category}
             </span>
             <img className="w-64 mx-auto transform transition duration-300 hover:scale-105" src={Item.details.image} alt="" />
             <div className="flex flex-col items-center my-3 space-y-2">
--- src/components/basic/Resturant.js
+++ src/components/basic/Resturant.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import Item from './Item';
 import Menu from './Menu';
-import MenuItems from './menuApi';
+import itemsFromApi from './menuApi';
 const Resturant = () => {
     const [menuItems, setMenuItems] = useState([]);
-    useEffect(() => setMenuItems(MenuItems));
+    useEffect(() => setMenuItems(itemsFromApi), []);
 
     return (
         <div className="my-12 max-w-screen-xl mx-auto px-6">
             <Menu />
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-12">
-                {MenuItems.map((item, index) =>
+                {menuItems.map((item) =>
                     <Item key={item.id} details={item} />
                 )}
 
