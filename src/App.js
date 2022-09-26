f2b02e5df70481e11070481a6afd7571e8cd5e37
Waqas Yousaf <iamwiqi@gmail.com>
2022-09-26 10:41:04 +0500

    Added router and dark mode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
-import Resturant from './components/basic/Resturant';
 import './App.css';
+import Form from './components/Form'
+import Nav from './components/Nav';
+import React, { useState } from 'react';
+import Toast from './components/Toast';
+import About from './components/About'
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [alert, setAlert] = useState(false);
+  const toggleDarkMode = () => {
+    let mode = darkMode ? false : true;
+    setDarkMode(mode);
+    if (mode)
+      toastie("Dark Mode has been enabled", "success");
+    else
+      toastie("Dark Mode has been Disabled", "success");
+  }
+
+  const toastie = (message, status) => {
+    let msg = { message: message, status: status };
+    setAlert(msg);
+    setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+  }
   return (
     <>
-      <Resturant />
+    <Router>
+      <Nav mode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <div className={`w-full ${darkMode && 'dark'}`}>
+        <Toast alert={alert} />
+
+          <Routes>
+            <Route path="/" element={<Form toastie={toastie} />}>
+              <Route index element={<Form />} />
+            </Route>
+
+            <Route path="/about" element={<About toastie={toastie} />}>
+
+            </Route>
+          </Routes>
+
+
+        <p className="dark:text-gray-700 dark:bg-gray-900 py-4 text-center text-gray-500 text-xs">
+          &copy;{new Date().getFullYear()} Wishdd.com. All rights reserved.
+        </p>
+      </div>
+      </Router>
     </>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+
+const Form = ({ toastie }) => {
+
+    const [text, setText] = useState("");
+
+    useEffect(() => {
+        setText("Hello World");
+    }, []);
+
+
+    function toLowerCase() {
+        setText(text.toLowerCase());
+        toastie("Text Converted to Lowercase", "success");
+    }
+
+    const toUpperCase = () => {
+        setText(text.toUpperCase());
+        toastie("Text Converted to Uppercase", "success");
+    }
+    const clearText = () => {
+        setText("");
+        toastie("Text Cleared", "success");
+    }
+
+    const copyText = () => {
+        navigator.clipboard.writeText(text);
+        toastie("Copied to clip board", "success");
+    }
+
+    return (
+
+        <>
+            <div className="dark:text-gray-700 dark:bg-gray-900 bg-white  px-8 pt-6 pb-8 ">
+                <div className="">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                        Enter Your Text
+                    </label>
+                    <textarea value={text} onChange={(e) => setText(e.target.value)} className="dark:text-gray-300 dark:bg-gray-800
+                    shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        placeholder="Some Cute Text" rows="12">
+                    </textarea>
+                </div>
+                <button onClick={toUpperCase} className="bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    To Upper Case
+                </button>
+                <button onClick={toLowerCase} className="ml-8 bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    To Lower Case
+                </button>
+                <button onClick={copyText} className="ml-8 bg-blue-700 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    Copy to Clipboard
+                </button>
+                <button onClick={clearText} className="ml-8 bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    Clear Text
+                </button>
+
+            </div>
+            <div className="bg-white dark:text-gray-700 dark:bg-gray-900  shadow-md px-8 pt-6 pb-8 ">
+                <h2 className="mb-3 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">
+                    Word Statistics</h2>
+                <p className="mb-3 text-gray-500 dark:text-gray-400">Total {text.length > 0 ? text.split(" ").length : 0} words or {text.length} characters</p>
+                <p className="mb-3 text-gray-500 dark:text-gray-400">Can be read in {text.split(" ").length * 0.008} minutes</p>
+            </div>
+            <div className="bg-white shadow-md  px-8 pt-6 pb-8   border shadow-md hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Preview</h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{text}</p>
+
+            </div>
+        </>
+
+    );
+}
+
+export default Form;
